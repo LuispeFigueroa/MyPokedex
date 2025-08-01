3ce3717368f3b88455dd420c748bc30647467f98
LuispeFigueroa <fig24087@uvg.edu.gt>
2025-08-01 14:02:06 -0600

    Paso 4, agregar composable CARd y algunas dependencias

--- app/src/main/java/com/uvg/mypokedex/ui/components/PokemonCard.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/components/PokemonCard.kt
@@ -1,4 +1,26 @@
 package com.uvg.mypokedex.ui.components
 
-class PokemonCard {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+ fun PokemonCard( ){
+ Card( modifier = Modifier
+   .padding(8.dp)
+   .fillMaxWidth(),
+  shape = MaterialTheme.shapes.medium,
+  elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
+  colors = CardDefaults.cardColors(
+   containerColor = typeColor
+  ){
+
+  }
+ )
 }
+
