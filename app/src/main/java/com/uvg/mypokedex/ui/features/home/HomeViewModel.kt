fde001cbca0e58557559dc60d07e793ea6ef5112
LuisPHernandez <lphm0105@gmail.com>
2025-08-02 17:20:12 -0600

    PokemonCard

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
@@ -8,8 +8,8 @@ class HomeViewModel {
     fun getPokemonList(): List<Pokemon> {
         return listOf(
             Pokemon(1, "Hierba / Veneno","Bulbasaur",50 ),
-        Pokemon(4, "Fuego", "Charmander", 60),
-        Pokemon(150, "Psiquico","Mewtwo",200),
+            Pokemon(4, "Fuego", "Charmander", 60),
+            Pokemon(150, "Psiquico","Mewtwo",200),
         )
     }
 
--- app/src/main/java/com/uvg/mypokedex/ui/features/home/MainActivity.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/MainActivity.kt
@@ -12,30 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.uvg.mypokedex.ui.theme.MyPokedexTheme
+import androidx.compose.foundation.layout.Column
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyPokedexTheme {
-                Scaffold { HomeScreen() }
+                Column {
+                    HomeScreen()
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyPokedexTheme {
-        Greeting("Android")
     }
 }
