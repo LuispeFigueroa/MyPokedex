e0927a566fd7947662ef89982dc6be1311932030
LuispeFigueroa <fig24087@uvg.edu.gt>
2025-08-22 21:50:39 -0600

    Hacer cambios a los atributos del pokemon y crear la data class Stat que permita almacenar diferentes estadisticas

--- app/src/main/java/com/uvg/mypokedex/data/model/Pokemon.kt
+++ app/src/main/java/com/uvg/mypokedex/data/model/Pokemon.kt
@@ -2,10 +2,15 @@ package com.uvg.mypokedex.data.model
 
 data class Pokemon(
     val id: Int,
-    val type: String,
+    val types: List<String>,
     val name: String,
     val health: Int,
-) {
-
-
-}
+    val stats: Stats,
+    val height: Float
+)
+data class Stats(
+    val Attack:Int,
+    val Defense:Int,
+    val SpecialAttack: Int,
+    val Speed: Int
+)
