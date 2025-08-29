81162acfd0cca18ce26a07e1c246afb1159a6b2c
LuisPHernandez <lphm0105@gmail.com>
2025-08-29 13:59:45 -0600

    Filtrar pokemons por nombre

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeScreen.kt
@@ -1,19 +1,65 @@
 package com.uvg.mypokedex.ui.features.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.unit.dp
+import com.uvg.mypokedex.data.model.Pokemon
 import com.uvg.mypokedex.ui.components.PokemonCard
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+fun searchPokemon(searchText: String, pokemonList: List<Pokemon>): List<Pokemon> {
+    if (searchText.isBlank()) {
+        return pokemonList
+    } else {
+        return pokemonList.filter { it.name.contains(searchText, ignoreCase = true) }
+    }
+}
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = HomeViewModel(),
 ) {
     val pokemonList = viewModel.getPokemonList()
+    var searchText by rememberSaveable { mutableStateOf("") }
+    var filteredPokemons by remember { mutableStateOf(pokemonList) }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        TextField(
+            value = searchText,
+            label = {
+                Text("Filtrar por nombre")
+            },
+            onValueChange = {
+                searchText = it
+                filteredPokemons = searchPokemon(searchText, pokemonList)
+            }
+        )
+        if (filteredPokemons.isEmpty()) {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "No se encontraron Pokemons"
+            )
+        }
+    }
 
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
@@ -21,9 +67,7 @@ fun HomeScreen(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(
-            items = pokemonList,
-        ) { p ->
+        items(filteredPokemons) { p ->
             PokemonCard(
                 pokemon = p,
             )
