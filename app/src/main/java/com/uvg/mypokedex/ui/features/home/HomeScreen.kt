964e00c533ab5bb11ea2dcce483cd9d509135aff
LuispeFigueroa <fig24087@uvg.edu.gt>
2025-08-30 20:07:52 -0600

    Agregar FAB al homescreen, agregar logica de boton para filtar

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeScreen.kt
@@ -3,11 +3,14 @@ package com.uvg.mypokedex.ui.features.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,56 +24,85 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.uvg.mypokedex.ui.components.OrderButton
 
-fun searchPokemon(searchText: String, pokemonList: List<Pokemon>): List<Pokemon> {
-    if (searchText.isBlank()) {
-        return pokemonList
-    } else {
-        return pokemonList.filter { it.name.contains(searchText, ignoreCase = true) }
+
+
+    fun searchPokemon(searchText: String, pokemonList: List<Pokemon>): List<Pokemon> {
+        if (searchText.isBlank()) {
+            return pokemonList
+        } else {
+            return pokemonList.filter { it.name.contains(searchText, ignoreCase = true) }
+        }
     }
-}
 
-@Composable
-fun HomeScreen(
-    viewModel: HomeViewModel = HomeViewModel(),
-) {
-    val pokemonList = viewModel.getPokemonList()
-    var searchText by rememberSaveable { mutableStateOf("") }
-    var filteredPokemons by remember { mutableStateOf(pokemonList) }
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-
+    @Composable
+    fun HomeScreen(
+        viewModel: HomeViewModel = HomeViewModel(),
     ) {
-        TextField(
-            value = searchText,
-            label = {
-                Text("Filtrar por nombre")
-            },
-            onValueChange = {
-                searchText = it
-                filteredPokemons = searchPokemon(searchText, pokemonList)
+        val pokemonList = viewModel.getPokemonList()
+        var searchText by rememberSaveable { mutableStateOf("") }
+        var isAscending by remember { mutableStateOf(true) } //estado de orden de los pokemones
+        val displayedPokemons =remember(pokemonList, searchText, isAscending){
+            searchPokemon(searchText, pokemonList)
+                .let{ filteredList ->
+                    if (isAscending){
+                        filteredList.sortedBy{it.name.lowercase() } //ordenar despues de que se haya buscado alguno por nombre
+                    }else{
+                        filteredList.sortedByDescending { it.name.lowercase() } //ordenar de forma descendiente
+                    }
+                }
+        }
+
+        Scaffold(
+            floatingActionButton = {
+                OrderButton(isCurrentlyAscending = isAscending,
+                    onClick = { isAscending = !isAscending}
+                )
             }
-        )
-        if (filteredPokemons.isEmpty()) {
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = "No se encontraron Pokemons"
-            )
-        }
-    }
+        ){
+            innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                ,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                TextField(
+                    value = searchText,
+                    label = {
+                        Text("Filtrar por nombre")
+                    },
+                    onValueChange = {
+                        newText ->
+                        searchText = newText
+                    }
+                )
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(filteredPokemons) { p ->
-            PokemonCard(
-                pokemon = p,
-            )
+                Spacer(modifier = Modifier.height(16.dp))
+                if (displayedPokemons.isEmpty()) {
+                    Text(
+                        modifier = Modifier.padding(16.dp),
+                        text = "No se encontraron Pokemons"
+                    )
+                }else{
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    ){
+                        items(
+                            items = displayedPokemons,
+                            key = {pokemon -> pokemon.id}
+                        ){ p ->
+                            PokemonCard(
+                                pokemon = p,
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
-}
