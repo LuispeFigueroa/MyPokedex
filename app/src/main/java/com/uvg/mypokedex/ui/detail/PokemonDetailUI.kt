e7aa3e15358b5bd181528a74d68145dd2e84b826
LuisPHernandez <lphm0105@gmail.com>
2025-09-17 12:33:24 -0600

    Delte unnecesary files

--- app/src/main/java/com/uvg/mypokedex/ui/detail/PokemonDetailUI.kt
+++
@@ -1,9 +0,0 @@
-package com.uvg.mypokedex.ui.detail
-
-import androidx.compose.runtime.Composable
-import com.uvg.mypokedex.data.model.Pokemon
-
-@Composable
-fun DetailScreen(pokemon: Pokemon){
-
-}
